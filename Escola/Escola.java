73e4b38f03d7fcf9281d0d3cbbc6250c13661d8a
Lumah Pereira <115045300+lumahloi@users.noreply.github.com>
2024-06-13 10:10:16 -0300

    Update Escola.java

--- Escola/Escola.java
+++ Escola/Escola.java
@@ -40,4 +40,43 @@ public class Escola {
 			return semestre;
 		}
 	}
+
+	public static class Aluno {
+		private String nome;
+		private String matricula;
+		private String email;
+		private int idade;
+
+		public void setMatricula(String matricula) {
+			this.matricula = matricula;
+		}
+
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
+
+		public void setIdade(int idade) {
+			this.idade = idade;
+		}
+
+		public String getMatricula(){
+			return matricula;
+		}
+		
+		public String getNome(){
+			return nome;
+		}
+
+		public String getEmail(){
+			return email;
+		}
+
+		public int getIdade(){
+			return idade;
+		}
+	}
 }

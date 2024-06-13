e5b6562060c647f1e85adf92099961af3ef50353
Lumah Pereira <115045300+lumahloi@users.noreply.github.com>
2024-06-13 10:17:05 -0300

    Update Main.java

--- Arquivo/Main.java
+++ Arquivo/Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-
         System.out.println("Hello world!");
+
         FileAppService fileAppService = new FileAppService();
         fileAppService.Processamento();
+
+        EscreverArquivo escrever = new EscreverArquivo();
+        escrever.EscreverNoArquivo("arquivoTeste.txt", "Estou escrevendo no arquivo!");
     }
 }
